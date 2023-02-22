e09dcc0a5b7fe5ad83a3b84a67c2dd2ad5a1eef3
Mohammad Omar Shihab <lonak.secret@gmail.com>
2023-02-22 14:20:09 +0000

    set defaultPic + del imgs

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,10 +2,13 @@ import * as React from 'react';
 import "./ProductCard.css"
 
 export default function MultiActionAreaCard({product:{name,price,picture}}) {
+  
+const defaultPic = picture ? picture : "logo-t.png"
+
   return (
     <div className="food-card">
     <div className="food-card-img-container">
-      <img src={picture} alt="Food Item" />
+      <img src={defaultPic} alt="Food Item" />
       <div className="food-card-title-container">
         <h3 className="food-card-title">{name}</h3>
         <p className="food-card-price">₪{price}</p>
