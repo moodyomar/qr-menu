715a356d2f97a8b0ec29920edb6d4280f224fa94
Mohammad Omar Shihab <lonak.secret@gmail.com>
2023-02-25 15:16:15 +0000

    content + responsivity fix

--- src/components/ProductsSlider/ProductsSlider.js
+++ src/components/ProductsSlider/ProductsSlider.js
@@ -38,7 +38,7 @@ const ProductsSlider = ({language,endPoint}) => {
         "name": contentLng.catTitle,
         "desc": contentLng.catDesc
       }} />
-      <Carousel responsive={responsive} autoPlay={true}
+      <Carousel responsive={responsive} autoPlay={true} showDots={true} swipeable={false}
         autoPlaySpeed={3000} partialVisible={false} rewind={true} rewindWithAnimation={true} rtl={true}>
         {contentLng.categories.map((category, i) => (
           <a href={`#${category.name}`}><CategoryCard key={i} category={category} endPoint={endPoint}/></a>
