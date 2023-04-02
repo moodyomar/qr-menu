cd558d0d61959777b495728127add507766fa8a1
Mohammad Omar Shihab <lonak.secret@gmail.com>
2023-04-02 17:28:00 +0300

    change carousel to native css

--- src/components/ProductsSlider/ProductsSlider.js
+++ src/components/ProductsSlider/ProductsSlider.js
@@ -1,51 +1,29 @@
 import './ProductsSlider.css'
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import heContent from "../../json/content-hr.json"
 import arContent from "../../json/content-ar.json"
 import { Title } from '../';
 import { CategoryCard } from '..';
 
-const ProductsSlider = ({language,endPoint}) => {
+const ProductsSlider = ({ language, endPoint }) => {
 
   const contentLng = language === 'Ar' ? arContent : heContent
 
-
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 4
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 3
-    }
-  };
-
   return (
 
-    <div className='ProductsSlider'>
-      <Title category={{
-        "name": contentLng.catTitle,
-        "desc": contentLng.catDesc
-      }} />
-      <Carousel responsive={responsive} autoPlay={true} showDots={true} swipeable={false} infinite={true}
-        autoPlaySpeed={3000} partialVisible={false} rewind={true} rewindWithAnimation={true} rtl={true}>
-        {contentLng.categories.map((category, i) => (
-          <a href={`#${category.name}`}><CategoryCard key={i} category={category} endPoint={endPoint}/></a>
-        ))}
-      </Carousel>
+<div className='ProductsSlider'>
+  <Title category={{
+    "name": contentLng.catTitle,
+    "desc": contentLng.catDesc
+  }} />
+  <div style={{WebkitOverflowScrolling: 'touch', display:'flex', width:'100%', overflowX:'auto', maxWidth: '100%'}}>
+    {contentLng.categories.map((category, i) => (
+      <div key={i} style={{ flex: '0 0 auto', width: '33%', marginRight: '10px' }}>
+        <a href={`#${category.name}`}><CategoryCard category={category} endPoint={endPoint} /></a>
+      </div>
+    ))}
+  </div>
+</div>
 
-    </div>
 
   )
 }
