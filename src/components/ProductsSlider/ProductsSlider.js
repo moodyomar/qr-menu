8c7d6efd40194f471d4cc999b7fa92dcea5d0a21
Mohammad Omar Shihab <lonak.secret@gmail.com>
2023-04-03 13:42:32 +0300

    fix scroll position on category click

--- src/components/ProductsSlider/ProductsSlider.js
+++ src/components/ProductsSlider/ProductsSlider.js
@@ -1,27 +1,37 @@
-import './ProductsSlider.css'
+import './ProductsSlider.css';
 import { Title } from '../';
 import { CategoryCard } from '..';
 
 const ProductsSlider = ({ contentLng, endPoint }) => {
+  const scrollOffset = 150; // Adjust this value to control the scroll position
+
+  const handleCategoryClick = (categoryName) => {
+    const target = document.getElementById(categoryName);
+    const offsetPosition = target.offsetTop - scrollOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      // behavior: "smooth"
+    });
+  };
 
   return (
-
-<div className='ProductsSlider'>
-  <Title category={{
-    "name": contentLng.catTitle,
-    "desc": contentLng.catDesc
-  }} />
-  <div style={{WebkitOverflowScrolling: 'touch', display:'flex', width:'100%', overflowX:'auto', maxWidth: '100%'}}>
-    {contentLng.categories.map((category, i) => (
-      <div key={i} style={{ flex: '0 0 auto', width: '33%', marginRight: '10px' }}>
-        <a href={`#${category.name}`}><CategoryCard category={category} endPoint={endPoint} /></a>
+    <div className='ProductsSlider'>
+      <Title category={{
+        "name": contentLng.catTitle,
+        "desc": contentLng.catDesc
+      }} />
+      <div style={{WebkitOverflowScrolling: 'touch', display:'flex', width:'100%', overflowX:'auto', maxWidth: '100%'}}>
+        {contentLng.categories.map((category, i) => (
+          <div key={i} style={{ flex: '0 0 auto', width: '33%', marginRight: '10px' }}>
+            <button className='scroll-btn' onClick={() => handleCategoryClick(category.name)}>
+              <CategoryCard category={category} endPoint={endPoint} />
+            </button>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-</div>
+    </div>
+  );
+};
 
-
-  )
-}
-
-export default ProductsSlider
+export default ProductsSlider;
